71153f809b1bbae381848c18da6b8b9cf7453d5d
Felipe Daher <felipe_daher29@hotmail.com>
2022-04-22 15:18:56 -0300

    Otimizado formulario de cadastro e Input

--- src/components/inputs/Inputs.jsx
+++ src/components/inputs/Inputs.jsx
@@ -1,5 +1,5 @@
-import { forwardRef } from 'react'
 import styled from 'styled-components'
+import { useController } from 'react-hook-form'
 
 const InputContainer = styled.div`
   width: 100%;
@@ -36,14 +36,19 @@ const errorMessage = {
   'string.min': 'A senha deve conter pelo menos 6(seis) caracteres.'
 }
 
-const Input = forwardRef(({label, error, ...props}, ref) => {
+const Input = ({label, name, control, defaultValue = '', ...props}) => {
+  const {
+    field: { value, onChange },
+    fieldState: { error }
+  } = useController({ name, control, defaultValue })
+
   return (
     <InputContainer>
       <StyledLabel>{label}</StyledLabel>
-      <StyledInput placeholder={label} error={error} {...props} ref={ref} />
+      <StyledInput placeholder={label} error={error} {...props} value={value} onChange={onChange} />
       {error && <ErrorLabel>{errorMessage[error.type] || error.message}</ErrorLabel>}
     </InputContainer>
   )
-})
+}
 
 export default Input
